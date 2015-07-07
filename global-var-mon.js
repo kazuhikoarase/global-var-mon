4f8d1a1a7cddd387de068f638ade3efe4dc4519d
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2015-07-07 22:14:00 +0900

    initial commit

---
+++ global-var-mon.js
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------
+//
+// Global Variable Monitor
+//
+// Copyright (c) 2015 Kazuhiko Arase
+//
+// URL: https://github.com/kazuhikoarase/global-var-mon
+//
+// Licensed under the MIT license:
+//  http://www.opensource.org/licenses/mit-license.php
+//
+//---------------------------------------------------------------------
+
+!function() {
+  var timeout = 1000;
+  var vars = {};
+  var capture = function() {
+    vars = {};
+    for (var k in window) {
+      vars[k] = true;
+    }
+    window.setTimeout(dumpAndCapture, timeout);
+  };
+  var dumpAndCapture = function() {
+    for (var k in window) {
+      if (!vars[k]) {
+        console.log('Global variable is detected: ' + k +
+            ' (' + typeof k + ')');
+      }
+    }
+    capture();
+  };
+  capture();
+}();
---
+++ readme.md
@@ -0,0 +1,7 @@
+Global Variable Monitor
+
+Usage:
+
+Just include at head of script tags.
+
+<script src="global-var-mon.js"></script>
